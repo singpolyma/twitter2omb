0858cad9b568f09cb87fd95ce69ba2d020805c83
Pranketh <singpolyma@sunkist.dreamhost.com>
2008-11-29 13:51:02 -0800

    better error on failed request

--- twitter_profile.rb
+++ twitter_profile.rb
@@ -4,5 +4,9 @@ require 'open-uri'
 require 'json'
 
 def twitter_profile(user)
-	JSON::parse(open("http://twitter.com/users/show/#{user}.json").read)
+	begin
+		JSON::parse(open("http://twitter.com/users/show/#{user}.json").read)
+	rescue Exception
+		warn 'That twitter user is protected. Cannot read profile.'
+	end
 end
