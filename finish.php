022b1805c4f9bba9a7217a30592e7538c8600ee7
Pranketh <singpolyma@sunkist.dreamhost.com>
2008-11-29 13:20:11 -0800

    even more being really careful. I guess we shouldn't trust the session store.

--- finish.php
+++ finish.php
@@ -2,6 +2,11 @@
 
 session_start();
 
+$_SESSION['profile_url'] = str_replace("'",'',$_REQUEST['profile_url']);
+$_SESSION['twitter_name'] = str_replace("'",'',$_REQUEST['twitter_name']);
+$_SESSION['request_token'] = str_replace("'",'',$_REQUEST['request_token']);
+$_SESSION['request_token_secret'] = str_replace("'",'',$_REQUEST['request_token_secret']);
+
 exec("./omb.rb '{$_SESSION['profile_url']}' '{$_SESSION['twitter_name']}' '{$_SESSION['request_token']}' '{$_SESSION['request_token_secret']}'", $r);
 
 file_put_contents('tokens/'.sha1($r[3]), implode("\n",$r));
