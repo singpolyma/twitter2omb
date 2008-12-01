1b4e2e4bde842d33476e7d9052e44fd413f656fa
Pranketh <singpolyma@sunkist.dreamhost.com>
2008-12-01 14:40:34 -0800

    poll more efficiently using only a single request to the twitter search api

--- poll.rb
+++ poll.rb
@@ -2,12 +2,27 @@
 
 ENV['GEM_PATH'] = '/home/singpolyma/.gems:/usr/lib/ruby/gems/1.8'
 
-users = []
+require 'rubygems'
+require 'json'
+require 'open-uri'
+require 'cgi'
+require 'ombpost'
+
+users = {}
+query = []
 
 `ls tokens/`.split(/\n/).each do |token|
-	users << open('tokens/' + token).read.split(/\n/)[3].split(/=/)[1]
+	data = open('tokens/' + token).read.split(/\n/)
+	user = data[3].split(/=/)[1]
+	users[data[3].split(/=/)[1]] = data
+	query << "from:#{user}"
 end
 
-users.each do |user|
-	warn `ruby next_statuses.rb '#{user}'`
+statuses = JSON::parse(open("http://search.twitter.com/search.json?since_id=#{open('last_id').read.to_i}&rpp=100&q=#{CGI::escape(query.join(' OR '))}").read)['results']
+
+statuses.reverse_each do |el|
+	config = users[el['from_user']]
+	omb_post(config[2], config[3], config[0], config[1], el['id'], el['text'])
 end
+
+File.new('last_id', 'w').write(statuses.shift['id']) if statuses.length > 0
