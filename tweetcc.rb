2298d75c3e263ab4ccc76f8a4160cde52fe28c3b
Stephen Paul Weber <singpolyma@singpolyma.net>
2009-03-16 21:47:39 -0400

    Remove exits

--- tweetcc.rb
+++ tweetcc.rb
@@ -15,40 +15,33 @@ def tweetcc(user)
 
 	if pd && (!attribution && !sharealike && !noncommercial && !noderivs)
 		return 'http://creativecommons.org/licenses/publicdomain/'
-		exit
 	end
 
 	if attribution && (!pd && !sharealike && !noncommercial && !noderivs)
 		return 'http://creativecommons.org/licenses/by/3.0/'
-		exit
 	end
 
 	# TWO CLAUSE
 
 	if attribution && sharealike && (!pd && !noncommercial && !noderivs)
 		return 'http://creativecommons.org/licenses/by-sa/3.0/'
-		exit
 	end
 
 	if attribution && noncommercial && (!pd && !sharealike && !noderivs)
 		return 'http://creativecommons.org/licenses/by-nc/3.0/'
-		exit
 	end
 
 	if attribution && noderivs && (!pd && !sharealike && !noncommercial)
 		return 'http://creativecommons.org/licenses/by-nc/3.0/'
-		exit
 	end
 
 	# THREE CLAUSE
 	if attribution && noncommercial && sharealike && (!pd && !noncommercial)
 		return 'http://creativecommons.org/licenses/by-nc-nd/3.0/'
-		exit
 	end
 
 	if attribution && noncommercial && noderivs && (!pd && !sharealike)
 		return 'http://creativecommons.org/licenses/by-nc-nd/3.0/'
-		exit
 	end
 
 	return 'http://twitter.com/tos'
